f54799d81ee68b6fb8c27e07603f05a0df2cf38b
João Vinicius <joaovinicius@lavid.ufpb.br>
2019-04-24 02:38:29 -0300

    Adicionado comando para install numpy e scipy

--- InterfaceJava/src/interfacejava/InterfaceJava.java
+++ InterfaceJava/src/interfacejava/InterfaceJava.java
@@ -5,11 +5,15 @@
  */
 package interfacejava;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -25,13 +29,36 @@ public class InterfaceJava extends Application {
         
         stage.setScene(scene);
         stage.show();
+        
     }
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        launch(args);
+        installDependencies();
+        launch(args);        
+    }
+    
+    private static void installDependencies(){
+        
+        System.out.println("Installing numpy and scipy...");
+        Runtime rt = Runtime.getRuntime();
+        try {
+            Process p = rt.exec("cmd.exe /c python -m pip install numpy scipy");
+            
+            BufferedReader br;
+            br = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            
+            String line;
+            
+            while((line = br.readLine()) != null){
+                System.out.println(line);
+            }
+            
+        } catch (IOException ex) {
+            JOptionPane.showMessageDialog(null, "Ocorreu um erro ao instalar dependências.");
+        }
     }
     
 }
